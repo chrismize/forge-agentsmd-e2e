c5bf9e084fcd7da9cbcf31d0eb2b1c59b7440409
Chris Mize <chris.mize@wwt.com>
2026-09-02 18:56:26 +0000

    Revert "Hand-authored PR (no forge branch) (#13)"
    This reverts commit 23d0398c34e53dcfe473b37abe947ea167387b82.

--- scripts/cleanup.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-// Hand-authored helper: deletes a directory using a shell command.
-const { exec } = require("child_process");
-
-const target = process.argv[2];
-exec(`rm -rf ${target}`, (err, stdout, stderr) => {
-  if (stderr) console.log(stderr);
-  console.log("done");
-});
